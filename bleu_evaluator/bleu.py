dff30de6c8fe9c050e639dcce669814a78b2a1f1
vancomm <peshekhonov.ip@gmail.com>
2023-12-06 04:00:42 +0300

    Refactor code

--- bleu_evaluator/bleu.py
+++ bleu_evaluator/bleu.py
@@ -83,11 +83,11 @@ def modified_precision(
     else:
         raise ValueError("n must be in range [1; 4]")
 
+    ref_finders = list(map(finder_cls.from_words, references))
     sum_clipped = 0
     for candidate in candidates:
         cand_finder = finder_cls.from_words(candidate)
         cand_ngrams = list(ngrams(candidate, n))
-        ref_finders = list(map(finder_cls.from_words, references))
         sum_clipped += sum(
             min(
                 cand_finder.ngram_fd[ngram],
@@ -148,48 +148,7 @@ def bleu_score(
 
     s = math.fsum(w * math.log(p) for w, p in zip(weights, ps))
     score = brevity_penalty * math.exp(s)
-    logger.info(score)
+
+    logger.info(f"{score = }")
+
     return score
-
-
-def main():
-    from .parse import tokenize
-
-    example_1 = (
-        ("the the the the the the the",),
-        ("The cat is on the mat", "There is a cat on the mat"),
-    )
-
-    example_2 = (
-        (
-            "It is a guide to action which ensures that the military always obeys the commands of the party.",
-            "It is to insure the troops forever hearing the activity guidebook that party direct.",
-        ),
-        (
-            "It is a guide to action that ensures that the military will forever heed Party commands.",
-            "It is the guiding principle which guarantees the military forces always being under the command of the Party.",
-            "It is the practical guide for the army always to heed the directions of the party.",
-        ),
-    )
-
-    examples: Sequence[tuple[Sequence[str], Sequence[str]]] = (example_1, example_2)
-
-    for candidates, references in examples:
-        references = list(map(tokenize, references))
-        for candidate in map(tokenize, candidates):
-            # for i in range(1, 5):
-            #     print(i, modified_ngram_precision(references, hypothesis, n=i))
-            # for i in range(1, 5):
-            #     print(i, float(modified_precision_score(references, [hypothesis], n=i)))
-            bleu = bleu_score(
-                references,
-                [candidate],
-                n=4,
-                smoothing_function=lambda fr: fr.numerator + 0.0000001 / fr.denominator,
-            )
-            print(f"{bleu = }")
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    main()
